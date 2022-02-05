fe4bb3ed860666bcabdd0a0028d6852a61945db6
starikovyaroslav <slava.com.com90@gmail.com>
2022-02-06 00:37:09 +0700

    feat: подключение файла app

---
+++ app.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const { errors } = require('celebrate');
+const router = require('./routes/index');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const errorHandler = require('./middlewares/errorHandler');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/moviesdb', {});
+
+const allowedCors = [
+  'http://localhost:3000',
+];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', 'true');
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.end();
+  }
+
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(cookieParser());
+app.use(requestLogger);
+app.use(router);
+app.use(errorLogger);
+app.use(errors());
+app.use(errorHandler);
+
+app.listen(PORT);
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,11 @@ const moviesRouter = require('./movies');
 const usersRouter = require('./users');
 const auth = require('../middlewares/auth');
 const { createUser, login } = require('../controllers/users');
+const { loginValidation, userValidation } = require('../middlewares/validation');
 const NotFoundError = require('../errors/NotFoundError');
 
-router.post('/signup', createUser);
-router.post('/signin', login);
+router.post('/signup', userValidation, createUser);
+router.post('/signin', loginValidation, login);
 
 router.use('/', auth, moviesRouter);
 router.use('/', auth, usersRouter);
