c23f2c954e3cb496398fffa888960d496793e1de
starikovyaroslav <slava.com.com90@gmail.com>
2022-02-05 21:11:37 +0700

    feat: создать контроллеры для movies

---
+++ controllers/movies.js
@@ -0,0 +1,68 @@
+const Movie = require('../models/movie');
+const NotFoundError = require('../errors/NotFoundError');
+const ValidationError = require('../errors/ValidationError');
+const ForbiddenError = require('../errors/ForbiddenError');
+
+const getMovies = (req, res, next) => {
+  Movie.find({})
+    .then((movies) => res.send(movies))
+    .catch(next);
+};
+
+const createMovie = (req, res, next) => {
+  const owner = req.user._id;
+  const {
+    movieId,
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    thumbnail,
+    nameRU,
+    nameEN,
+  } = req.body;
+
+  Movie.create({
+    movieId,
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    thumbnail,
+    nameRU,
+    nameEN,
+    owner,
+  })
+    .then((movie) => res.send(movie))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        throw new ValidationError('Переданы некорректные данные при создании фильма');
+      }
+    })
+    .catch(next);
+};
+
+const deleteMovie = (req, res, next) => {
+  Movie.findById(req.params._id)
+    .orFail(new NotFoundError('Фильм с указанным id не найден'))
+    .then((movie) => {
+      if (!movie.owner.equals(req.user._id)) {
+        throw new ForbiddenError('Нельзя удалить чужой фильм');
+      }
+      return movie.remove()
+        .then(() => res.send('Фильм удален'));
+    })
+    .catch(next);
+};
+
+module.exports = {
+  getMovies,
+  createMovie,
+  deleteMovie,
+};
